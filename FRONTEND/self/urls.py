fcdd36612e40b0f13074765d309bf05f71f12c37
Mohan <mohankrishna28803@gmail.com>
2025-07-15 12:17:14 +0530

    commit

--- FRONTEND/self/urls.py
+++ FRONTEND/self/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os  # ✅ Import this
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('webapp.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
